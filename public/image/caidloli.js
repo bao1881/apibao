11f2a0d585f4009fa79f63cdd1a5599edca1b3ae
bao1881 <110312966+bao1881@users.noreply.github.com>
2022-09-11 17:34:07 +0000

    first commit

---
+++ public/image/caidloli.js
@@ -0,0 +1,17 @@
+exports.name = '/images/caidloli';
+exports.index = async(req, res, next) => {
+    const fs = require('fs-extra');
+    try {
+       // if (require('../API_KEY/data/check_api_key.js').check_api_key(req, res)) return;
+        let dirPath = __dirname + `/data/txt/caidloli.txt`;
+        var imageList = (fs.readFileSync(dirPath, "utf-8").split(/\r?\n/));
+        var randomImage = imageList[Math.floor(Math.random() * imageList.length)].trim();
+        res.jsonp({
+            data: randomImage,
+            count: imageList.length,
+            author: 'Bao Nguyen'
+        });
+    } catch (e) {
+        return res.jsonp({ error: e });
+    }
+}
